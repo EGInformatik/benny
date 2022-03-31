397ca10d58fe26063dd29f9800a425d7c32902f1
uldahlalex <41024316+uldahlalex@users.noreply.github.com>
2022-03-31 12:42:46 +0200

    benny refresh

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-k = 0;
+function aarstal() {
+    let aar1 = 2022;
+    let aar2 = 1995;
 
-function doSomething() {
-    {
-        if(k%3==0) {
-        $("h1").css("color", "#0088ff");
-        k = k + 1;
-        console.log(k);
-    } else if (k%3==1) {
-        $("h1").css("color", "#00ff00");
-        k = k + 1;
-        console.log(k);
-    } else if(k%3==2) {
-        $("h1").css("color", "#ff0000");
-        k = k + 1;
-        console.log(k);
-    }
-}
-
+    let result = aar1 - aar2;
+    $("p").append(result);
 }
